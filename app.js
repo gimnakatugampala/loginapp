de06b529f483adb156883efe47fd3464ea7510ba
gimnakatugampala <gimnakatugampala1@gmail.com>
2021-06-14 01:09:12 +0530

    app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+const port = process.env.PORT || 3000
+app.listen(() =>{
+    console.log(`server started at : ${port}`)
+},port)
