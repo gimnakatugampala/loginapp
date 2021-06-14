edc5bf804dccfa7a821031f8289de4e37cae6bb0
gimnakatugampala <gimnakatugampala1@gmail.com>
2021-06-14 11:57:31 +0530

    added

--- router/userRouter.js
+++ router/userRouter.js
@@ -9,4 +9,18 @@ router.get('/register',function(req,res){
     res.render('register')
 })
 
+router.post('/register',function(req,res){
+    const first_name = req.body.first_name;
+    const last_name = req.body.last_name;
+    const email = req.body.email;
+    const password = req.body.password;
+    const password2 = req.body.password2;
+    console.log(first_name ,last_name , email ,password ,password2)
+})
+
+router.get('/logout',function(req,res){
+    res.redirect('/users/login')
+})
+
+
 module.exports = router;
